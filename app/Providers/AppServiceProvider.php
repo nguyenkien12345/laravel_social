286484e20337819a14c88b042a6ce47704b859c9
Nguyen Trung Kien <nguyenkien11202000@gmail.com>
2023-08-06 21:30:32 +0700

    Upload Project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(app()->environment('remote')) {
+            URL::forceScheme('https');
+        }
     }
 }
